ae3acce46eb24d1dc115fcdda1a6e33fc1abacfd
Nelson Daza <nelson.daza@gmail.com>
2021-11-01 02:24:26 -0500

    minify CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { getConfig } = require('./config/utils')
 
 const build = getConfig('build')
 
 module.exports = {
-  plugins: [build.enableTailwindCSS && require('tailwindcss'), require('autoprefixer')].filter(Boolean),
+  plugins: [
+    build.enableTailwindCSS && require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ],
+    }),
+  ].filter(Boolean),
 }
