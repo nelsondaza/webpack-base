887a4264fdaf19774e6c496b51924c7cee043ab7
Nelson Daza <nelson.daza@gmail.com>
2021-11-12 01:39:38 -0500

    add heroku scripts

---
+++ config/scripts/heroku-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -f ./config/.env.yml ];
+  then
+    cp ./config/.env.example.yml ./config/.env.yml;
+fi
+
+# test app
+yarn test
+
+# build app
+yarn webpack --mode production --bail
+
+# build stories
+yarn build-storybook -c config/storybook -o dist/public/stories
